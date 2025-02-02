8d8e3e65531d6057c8def3fefc9bddb48342ce47
Hane Hatem <hanehatemelbasuony20@gmail.com>
2025-02-02 14:02:12 +0200

    not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,17 @@
+import scarecrow from '../assets/Scarecrow.png';
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="display">
+      <div className="display__img">
+        <img src={scarecrow} alt="404-Scarecrow" />
+      </div>
+      <div className="display__content">
+        <h2 className="display__content--info">I have bad news for you</h2>
+        <p className="display__content--text">
+          The page you are looking for might be removed or is temporarily
+          unavailable
+        </p>
+      </div>
+    </div>
+  );
 }
